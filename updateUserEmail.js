0ec0b42ceded231c88f0402a5dd771ddd13471a5
Giancarlo Byrne <gbyrne@razoyo.com>
2024-10-07 09:17:02 -0500

    initial

---
+++ updateUserEmail.js
@@ -0,0 +1,32 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const uid = "vA12xxx"; // Specify the user ID
+admin
+  .auth()
+  .getUser(uid)
+  .then(function (userRecord) {
+    // Update the user's email
+    return updateUserEmail(userRecord, "<email>");
+  })
+  .catch(function (error) {
+    console.log("Error fetching user:", error);
+  });
+
+function updateUserEmail(userRecord, newEmail) {
+  return admin
+    .auth()
+    .updateUser(userRecord.uid, {
+      email: newEmail,
+    })
+    .then(function (updatedUserRecord) {
+      console.log("User email updated:", updatedUserRecord.email);
+    })
+    .catch(function (error) {
+      console.log("Error updating user email:", error);
+    });
+}
